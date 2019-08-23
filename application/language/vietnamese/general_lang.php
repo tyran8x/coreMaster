6f3ec4877c65b02fa831882c8857cad9245e1a52
tyran8x <tyran8x@gmail.com>
2019-08-23 20:57:46 +0700

    Initial commit

---
+++ application/language/vietnamese/general_lang.php
@@ -0,0 +1,26 @@
+<?php
+
+	$lang['website_name'] = "My Website";
+	$lang['current_language'] = "Ngôn ngữ hiện tại";
+
+	// Menu
+	$lang['home'] = "Trang chủ";
+	$lang['about_us'] = "Giới thiệu";
+	$lang['contact_us'] = "Liên hệ";
+	$lang['example'] = "Ví dụ";
+
+	// General terms
+	$lang['address'] = "Địa chỉ";
+	$lang['email'] = "Email";
+	$lang['phone'] = "Điện thoại";
+	$lang['remarks'] = "Nhận xét";
+	$lang['subject'] = "Chủ đề";
+	$lang['message'] = "Tin nhắn";
+	
+	// Actions
+	$lang['sign_up'] = "Đăng ký";
+	$lang['login'] = "Đăng nhập";
+	$lang['search'] = "Tìm kiếm";
+	$lang['send'] = "Gửi";
+	$lang['download'] = "Tải xuống";
+	$lang['read_more'] = "Xem thêm";
---
+++ application/modules/admin/controllers/ChauLuc.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class ChauLuc extends Admin_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('form_builder');
+	}
+
+	// Frontend User CRUD
+	public function index()
+	{
+		$crud = $this->generate_crud('chauluc');
+		$crud->set_subject('Châu lục');
+		$crud->columns('ma', 'tentiengviet', 'tentienganh');
+		$crud->display_as('ma','Mã');
+        $crud->display_as('tentiengviet','Tên tiếng việt');
+        $crud->display_as('tentienganh','Tên tiếng anh');
+
+		$this->mPageTitle = 'Châu lục';
+		$this->render_crud();
+	}
+
+}
